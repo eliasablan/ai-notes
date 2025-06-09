31488cf11dab3c48a7c602427a7a3effa9f85179
Elias Ablan <eliasablan93@gmail.com>
2025-06-09 19:31:34 +0200

    fix header background color

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,37 @@
 import Link from "next/link";
 import { ThemeDropdown } from "./theme-dropdown";
 import { Notebook } from "lucide-react";
+import { Suspense } from "react";
+import { Button } from "./ui/button";
 
 export default function Header() {
+  const user = null;
   return (
-    <header className="bg-popover relative flex h-24 w-full items-center justify-between px-4 xl:px-8">
+    <header className="bg-secondary relative flex h-20 w-full items-center justify-between px-4 xl:px-8">
       <Link
         href="/"
-        className="hover:text-muted-foreground flex items-center gap-2 duration-500"
+        className="hover:text-muted-foreground flex items-end gap-2 duration-500"
       >
-        <Notebook className="size-8" />
-        <span className="text-2xl font-bold">Goat Notes</span>
+        <Notebook className="size-9" />
+        <h1 className="flex flex-col text-xl leading-none font-semibold">
+          Goat <span>Notes</span>
+        </h1>
       </Link>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-2">
+        <Suspense fallback={<div>Cargando...</div>}>
+          {user ? (
+            <Button variant="outline">Logout</Button>
+          ) : (
+            <>
+              <Button asChild>
+                <Link href="/ingresar">Ingresa</Link>
+              </Button>
+              <Button variant="outline" className="hidden md:block" asChild>
+                <Link href="/registrar">Regístrate</Link>
+              </Button>
+            </>
+          )}
+        </Suspense>
         <ThemeDropdown />
       </div>
     </header>
