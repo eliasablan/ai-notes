0eb1bd87f39fcd4cb19563c5dccb9c2f5819da4e
Elias Ablan <eliasablan93@gmail.com>
2025-06-11 04:26:32 +0200

    middleware final set up with route handlers to go to latest note or create new one

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,40 +23,76 @@ export async function updateSession(request: NextRequest) {
     request,
   });
 
-  console.log("middleware flag");
+  const supabase = createServerClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) =>
+            request.cookies.set(name, value),
+          );
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options),
+          );
+        },
+      },
+    },
+  );
 
-  // const supabase = createServerClient(
-  //   process.env.SUPABASE_URL!,
-  //   process.env.SUPABASE_ANON_KEY!,
-  //   {
-  //     cookies: {
-  //       getAll() {
-  //         return request.cookies.getAll();
-  //       },
-  //       setAll(cookiesToSet) {
-  //         cookiesToSet.forEach(({ name, value }) =>
-  //           request.cookies.set(name, value),
-  //         );
-  //         supabaseResponse = NextResponse.next({
-  //           request,
-  //         });
-  //         cookiesToSet.forEach(({ name, value, options }) =>
-  //           supabaseResponse.cookies.set(name, value, options),
-  //         );
-  //       },
-  //     },
-  //   },
-  // );
+  const isAuthRoute =
+    request.nextUrl.pathname.startsWith("/ingresar") ||
+    request.nextUrl.pathname.startsWith("/registrar");
 
-  // Do not run code between createServerClient and
-  // supabase.auth.getUser(). A simple mistake could make it very hard to debug
-  // issues with users being randomly logged out.
+  if (isAuthRoute) {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
 
-  // IMPORTANT: DO NOT REMOVE auth.getUser()
+    if (user) {
+      return NextResponse.redirect(
+        new URL("/", process.env.NEXT_PUBLIC_APP_URL!),
+      );
+    }
+  }
 
-  // const {
-  //   data: { user },
-  // } = await supabase.auth.getUser();
+  const { searchParams, pathname } = new URL(request.url);
+
+  if (!searchParams.get("noteId") && pathname === "/") {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    if (user) {
+      const { newestNoteId } = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/fetch-newest-note?userId=${user.id}`,
+      ).then((res) => res.json());
+
+      if (newestNoteId) {
+        const url = request.nextUrl.clone();
+        url.searchParams.set("noteId", newestNoteId);
+        return NextResponse.redirect(url);
+      } else {
+        const { noteId } = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/create-new-note?userId=${user.id}`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          },
+        ).then((res) => res.json());
+        const url = request.nextUrl.clone();
+        url.searchParams.set("noteId", noteId);
+        return NextResponse.redirect(url);
+      }
+    }
+  }
 
   return supabaseResponse;
 }
